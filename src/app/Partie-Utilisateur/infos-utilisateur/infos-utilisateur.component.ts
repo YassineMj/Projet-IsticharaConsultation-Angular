2cb8e2ae983e28dad5c1f1615ccd2e4e84af2655
Oumaima0762 <ouma.hajj123@gmail.com>
2024-04-03 13:43:54 +0000

    3 new component InfosConsulation , paiement , infosUtilisateur

--- src/app/Partie-Utilisateur/infos-utilisateur/infos-utilisateur.component.ts
+++ src/app/Partie-Utilisateur/infos-utilisateur/infos-utilisateur.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-infos-utilisateur',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./infos-utilisateur.component.css']
 })
 export class InfosUtilisateurComponent {
+  constructor(
+    private router: Router,
+  
+  ) { }
+  
+ouvrirNouvelOnglet() {
+    const url = this.router.serializeUrl(
+      this.router.createUrlTree(['/termes-conditions'])
+    );
+    window.open(url, '_blank');
+  }
 
 }
