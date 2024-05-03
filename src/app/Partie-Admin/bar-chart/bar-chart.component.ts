0939a99ce17ffabec02b298c0c0e1d1c5342a186
Bouchra <benseddik.bou@gmail.com>
2024-05-03 15:27:38 +0100

    dashboard

---
+++ src/app/Partie-Admin/bar-chart/bar-chart.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit, Input } from '@angular/core';
+import Chart from 'chart.js/auto';
+
+
+@Component({
+  selector: 'app-bar-chart',
+  templateUrl: './bar-chart.component.html',
+  styleUrls: ['./bar-chart.component.css'],
+})
+export class BarChartComponent implements OnInit {
+  constructor() {}
+  @Input() labels: String[] = [];
+  @Input() data1: number[] = [];
+  @Input() data2: number[] = [];
+  ngOnInit(): void {
+    this.createBarChart();
+  }
+
+  createBarChart(): void {
+    const data = {
+      labels: this.labels, // Remplacez par vos domaines
+      datasets: [
+        {
+          label: 'Nombre de catégories par domaine',
+          data: this.data1, // Remplacez par le nombre de catégories par domaine
+          backgroundColor: 'rgba(255, 99, 132, 0.5)',
+          borderColor: 'rgba(255, 99, 132, 1)',
+          borderWidth: 1,
+        },
+        {
+          label: 'Nombre de consultants par domaine',
+          data: this.data2,//consultants
+          backgroundColor: 'rgba(54, 162, 235, 0.5)',
+          borderColor: 'rgba(54, 162, 235, 1)',
+          borderWidth: 1,
+        },
+      ],
+    };
+
+    const options = {
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+      },
+    };
+
+    const ctx = document.getElementById('barChart') as HTMLCanvasElement;
+    new Chart(ctx, {
+      type: 'bar',
+      data: data,
+      options: options,
+    });
+  }
+}
--- src/app/Partie-Admin/dashboard-admin/dashboard-admin.component.ts
+++ src/app/Partie-Admin/dashboard-admin/dashboard-admin.component.ts
@@ -3,8 +3,25 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-dashboard-admin',
   templateUrl: './dashboard-admin.component.html',
-  styleUrls: ['./dashboard-admin.component.css']
+  styleUrls: ['./dashboard-admin.component.css'],
 })
 export class DashboardAdminComponent {
-
+  labels = [
+    'Domaine 1',
+    'Domaine 2',
+    'Domaine 3',
+    'Domaine 4',
+    'Domaine 5',
+    'Domaine 6',
+  ];
+  labelsdomaines = [
+    'Domaine 1',
+    'Domaine 2',
+    'Domaine 3',
+    'Domaine 4',
+    'Domaine 5',
+    'Domaine 6',
+  ];
+  datacategories = [2, 8, 5, 4, 3, 7];
+  dataconsultants = [5, 6, 10, 7, 12, 13];
 }
